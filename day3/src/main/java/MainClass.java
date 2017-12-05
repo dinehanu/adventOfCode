12024e7ecb4fa4902379f610529fae5d4ae40d2b
Daniel Mattsson <dmattsso88@gmail.com>
2017-12-05 18:49:27 +0100

    ändring

--- day3/src/main/java/MainClass.java
+++ day3/src/main/java/MainClass.java
@@ -6,7 +6,12 @@ public class MainClass {
     public static void main(String[] args) {
     SpiralMemory memory = new SpiralMemory();
 
-    System.out.println("circleIndex: " + memory.getRowNumber(265149));
+    int input = 265149;
+        int test = 1024;
+
+    System.out.println("Input: " + input + "\ncircleIndex: " + memory.calculateNumberOfSteps(input));
+
+        System.out.println("Input: " + test + "\ncircleIndex: " + memory.calculateNumberOfSteps(test));
 
     }
 
--- day3/src/main/java/SpiralMemory.java
+++ day3/src/main/java/SpiralMemory.java
@@ -2,20 +2,51 @@ public class SpiralMemory {
 
     public int calculateNumberOfSteps(int position){
 
-        int rowValue = 1;
-        int rowIndex = 0;
+        int circleNumber = getCircleNumber(position);
+        int lastIndex = getLastIndexOfCircle(position);
 
-        rowValue += (4*(rowIndex + 2));
+        if(circleNumber == 0){
+            return 0;
+        }
+        int numberOfSteps = (circleNumber == 1 ? 2 : (circleNumber^2));
+
+
+            if(lastIndex == position){
+                return numberOfSteps;
+            }
+            else{
+                int stepCounter = 1;
+                int xCounter = 4;
+                int yCounter = 0;
+                while(lastIndex != position){
+
+                }
+
+            }
+
         return 0;
     }
 
-    public int getRowNumber(int index){
-        int ret = 0;
+    public int getCircleNumber(int index){
+        int circleNumber = 0;
         int firstIndexInCircle = 1;
+
+        int increase = 0;
         while(firstIndexInCircle < index){
-            firstIndexInCircle += (firstIndexInCircle+8);
-            ret++;
+            increase += 8;
+            firstIndexInCircle += increase;
+            circleNumber++;
         }
-        return ret-1;
+        return circleNumber;
+    }
+
+    public int getLastIndexOfCircle(int index){
+        int lastIndexInCircle = 1;
+        int increase = 0;
+        while(lastIndexInCircle < index){
+            increase += 8;
+            lastIndexInCircle += increase;
+        }
+        return lastIndexInCircle;
     }
 }
