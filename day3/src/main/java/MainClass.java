adea8e5f1e7f94edcc89c61980801a788355e72f
Mattsson Daniel <daniel.mattsson@lm.se>
2017-12-05 16:16:08 +0100

    Lite mer

--- day3/src/main/java/MainClass.java
+++ day3/src/main/java/MainClass.java
@@ -6,7 +6,13 @@ public class MainClass {
     public static void main(String[] args) {
     SpiralMemory memory = new SpiralMemory();
 
-    System.out.println("circleIndex: " + memory.getRowNumber(265149));
+    //System.out.println("circleIndex with index " + 265149 + ": " + memory.getRowNumber(265149));
+        //System.out.println("circleIndex with index " + 1024 + ": " + memory.getRowNumber(1024));
+
+        int[] positions = memory.getPositions(3, 25, 12);
+        System.out.println("circleIndex with index " + 1024 + ": ");
+        System.out.println("X = " + positions[0]);
+        System.out.println("y = " + positions[1]);
 
     }
 
--- day3/src/main/java/SpiralMemory.java
+++ day3/src/main/java/SpiralMemory.java
@@ -10,12 +10,88 @@ public class SpiralMemory {
     }
 
     public int getRowNumber(int index){
-        int ret = 0;
+        int ret = 1;
         int firstIndexInCircle = 1;
+        int increase = 0;
         while(firstIndexInCircle < index){
-            firstIndexInCircle += (firstIndexInCircle+8);
+            increase += 8;
+            firstIndexInCircle += increase;
             ret++;
         }
         return ret-1;
+    }
+
+    public int[] getPositions(int circleNumber, int largestIndex, int index){
+
+        int[] ret;
+
+        ret = checkXAndY(circleNumber, largestIndex, index);
+        if(ret[0] != circleNumber || ret[1] != circleNumber ){
+            return ret;
+        }
+
+        ret = checkXAndY(circleNumber, (largestIndex - circleNumber*2), index);
+        if(ret[0] != circleNumber || ret[1] != circleNumber ){
+            return ret;
+        }
+
+        ret = checkXAndY(circleNumber, (largestIndex - circleNumber*4), index);
+        if(ret[0] != circleNumber || ret[1] != circleNumber ){
+            return ret;
+        }
+
+        ret = checkXAndY(circleNumber, (largestIndex - circleNumber*6), index);
+        if(ret[0] != circleNumber || ret[1] != circleNumber ){
+            return ret;
+        }
+
+
+        /**
+        int numberOfSteps = circleNumber * 2;
+
+        int counter = 0;
+        int x = circleNumber;
+        int y = circleNumber;
+
+        if(index == largestIndex){
+            return new int[]{x,y};
+        }
+
+            //No change in y
+            while(counter <= numberOfSteps){
+                x = counter <= circleNumber ? x-- : x++;
+                largestIndex--;
+                if(largestIndex == index){
+                    return new int[]{x,y};
+                }
+            }
+        //No change in x
+        while(counter <= numberOfSteps){
+            y = counter <= circleNumber ? y-- : y++;
+            largestIndex--;
+            if(largestIndex == index){
+                return new int[]{x,y};
+            }
+        }
+
+         */
+        return ret;
+    }
+
+    public int[] checkXAndY(int circleNumber, int largestIndex, int index){
+
+        int counter = 0;
+        int x = circleNumber;
+        int y = circleNumber;
+        int numberOfSteps = circleNumber * 2;
+
+        while(counter <= numberOfSteps){
+            y = counter <= circleNumber ? y-- : y++;
+            largestIndex--;
+            if(largestIndex == index){
+                return new int[]{x,y};
+            }
+        }
+        return new int[]{x,y};
     }
 }
