7add6e8fd1135fcefb4467d138316c1aff569dd3
Daniel Mattsson <dmattsso88@gmail.com>
2018-12-04 21:50:18 +0100

    Fixat steg 1 dag 4

--- 2018/day3/src/main/java/se/adventofcode/solver/OverlapCalculator.java
+++ 2018/day3/src/main/java/se/adventofcode/solver/OverlapCalculator.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 @Component
 public class OverlapCalculator {
---
+++ 2018/day4/src/test/java/se/adventofcode/solvers/Day4SolverTest.java
@@ -0,0 +1,23 @@
+package se.adventofcode.solvers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Day4SolverTest {
+
+    @Autowired
+    Day4Solver solver;
+
+    @Test
+    public void runStrategyOne(){
+    assertEquals(240, solver.runStrategyOne("input"));
+    }
+
+}
