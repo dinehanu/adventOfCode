dc6a7ba0bf9ec4cdfb83a839453728e3be070682
Daniel Mattsson <dmattsso88@gmail.com>
2018-12-09 17:17:51 +0100

    Rejält optimerad lösning

--- 2018/day9/src/main/java/se/adventofcode/solver/Day9Solver.java
+++ 2018/day9/src/main/java/se/adventofcode/solver/Day9Solver.java
@@ -1,59 +1,49 @@
 package se.adventofcode.solver;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class Day9Solver {
 
-    public long solveFirstTask(int numberOfPlayers, int lastMarbleValue){
+    public long solveFirstTask(int numberOfPlayers, int lastMarbleValue) {
         return getGamePoints(numberOfPlayers, lastMarbleValue);
     }
 
-    private long getGamePoints(int numberOfPlayers, int lastMarbleValue){
-
-        List<Integer> players = new ArrayList<>(Collections.nCopies(numberOfPlayers, 0));
-        int currentMarbleIndex = 0;
-        int currentPlayer = 0;
-
-        List<Integer> playingField = new ArrayList<>();
-        playingField.add(0);
-
+    private long getGamePoints(int numberOfPlayers, int lastMarbleValue) {
+        CircleDeque<Integer> circle = new CircleDeque<>();
+        circle.addFirst(0);
+        long[] scores = new long[numberOfPlayers];
         for (int i = 1; i <= lastMarbleValue; i++) {
+            if (i % 100000 == 0) {
+                System.out.println("På värde: " + i);
+            }
             if (i % 23 == 0) {
-                int index6 = getIndex(currentMarbleIndex-6, playingField);
-                int index7 = getIndex(currentMarbleIndex-7, playingField);
-                int score = i + playingField.get(index7);
-                players.set(currentPlayer, players.get(currentPlayer) + score);
-                currentMarbleIndex = index6-1;
-                playingField.remove(index7);
+                circle.rotate(-7);
+                scores[i % numberOfPlayers] += i + circle.pop();
+
             } else {
-                int index = getAddIndex(currentMarbleIndex, playingField);
-                if(index >=playingField.size()){
-                    playingField.add(i);
-                    currentMarbleIndex = playingField.size()-1;
-                } else {
-                    playingField.add(index, i);
-                    currentMarbleIndex = index;
+                circle.rotate(2);
+                circle.addLast(i);
+            }
+        }
+        return Arrays.stream(scores).max().getAsLong();
+    }
+
+    class CircleDeque<T> extends ArrayDeque<T> {
+        void rotate(int num) {
+            if (num > 0) {
+                for (int i = 0; i < num; i++) {
+                    T t = this.removeLast();
+                    this.addFirst(t);
+                }
+            } else {
+                for (int i = 0; i < Math.abs(num) - 1; i++) {
+                    T t = this.remove();
+                    this.addLast(t);
                 }
             }
-            currentPlayer = currentPlayer == numberOfPlayers-1 ? 0 : currentPlayer+1;
-        }
-        return Collections.max(players);
-    }
 
-    private int getIndex(int index, List<Integer> playingField){
-        return index < 0 ? playingField.size() + index : index;
-    }
-
-    private int getAddIndex(int index, List<Integer> playingField){
-        if(index == playingField.size()-1){
-            return 1;
-        } else if(index == playingField.size()-2){
-           return playingField.size()+1;
-        } else {
-            return index+2;
         }
     }
-
 }
+
